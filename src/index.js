e2fe0c4454462fcef4937c875a56f4285027ea00
Bhumica-jaiswal <bhumicajaiswal@gmail.com>
2025-09-06 12:41:37 +0530

    mongo stuff

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import mongoose from 'mongoose'
+import connectDB from './db/index.js'
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//iife
+// ;(async()=>{
+//     try {
+//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     } catch (error) {
+//         console.log("Error:",error)
+//         throw error
+//     }
+// })()
